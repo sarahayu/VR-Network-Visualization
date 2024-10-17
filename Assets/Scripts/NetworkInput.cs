a875cb3b6e4298de5189935cf532f5edda4cbf13
sarahayu <sarahhhalcyon@gmail.com>
2024-10-17 01:59:46 -0700

    input

--- Assets/Scripts/NetworkInput.cs
+++ Assets/Scripts/NetworkInput.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
 
 namespace VidiGraph
 {
     public class NetworkInput : MonoBehaviour
     {
+
+        public XRInputValueReader<float> GripInput = new XRInputValueReader<float>("Grip");
+
         public void Initialize()
         {
         }
+
+        void OnEnable()
+        {
+            GripInput?.EnableDirectActionIfModeUsed();
+        }
+
         // Start is called before the first frame update
         void Start()
         {
-
         }
 
         // Update is called once per frame
         void Update()
         {
+
+            if (GripInput != null)
+            {
+                var gripVal = GripInput.ReadValue();
+                print(gripVal);
+            }
         }
     }
 
