9cb8ed7e91fcf015d3f3c53707fdc5d4a6a2cdfd
sarahayu <sarahhhalcyon@gmail.com>
2025-04-14 15:34:33 -0700

    typo

--- Assets/Voice/Scripts/Query.cs
+++ Assets/Voice/Scripts/Query.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using vidiGraph;
+using VidiGraph;
 using UnityEngine.UI;
 
 public class Query : MonoBehaviour
 {
 
     // TODO: Required Queries, 
-    public void exec_query(NodeCollection Nodes, List<Link> Links, string Query){
+    public void exec_query(NodeCollection Nodes, List<Link> Links, string Query)
+    {
         // should do the database query here
         Debug.Log("Query: " + Query);
     }
